c137bebb7b1ff40546306a6b346b3d1af04ab1ed
Michael Seddon <michael.seddon@ixxus.com>
2017-05-18 16:34:00 +0100

    IXXTRAIN-5 - Cleanup

--- hlv-store/src/main/scala/com/ixxus/hastalavista/service/SearchService.scala
+++ hlv-store/src/main/scala/com/ixxus/hastalavista/service/SearchService.scala
@@ -10,27 +10,35 @@ class SearchService extends AbstractService {
     def findPage(url: String): Set[Page] = pageStore.pages.filter(p => p.url == url)
 
     def find(term: String): List[(Page, Int)] = {
+        val searchTerms = term.split(' ')
+        if (searchTerms.size > 1) findMultipleWords(pageStore.pages, searchTerms)
+        else findSingleWord(pageStore.pages, term)
+    }
+
+    val findMultipleWords = (pages: Set[Page], searchTerms: Array[String]) => {
+
         var distances: List[(Page, Int)] = List()
 
-        val searchTerms = term.split(' ')
-        if (searchTerms.size > 1) {
-            pageStore.pages.foreach(p => {
-                val what = searchTerms.map(kw => (kw, p.delimContents.indexOf(kw))).filter(_._2 >= 0).toList
-                if (what.nonEmpty) {
-                    val dist = distCalc(searchTerms.toSeq.toList, what, 0, 0, what.head._2, 0)
-                    if (dist > 0) distances = (p, dist) :: distances
-                }
-            })
+        pageStore.pages.foreach(p => {
+            val indexes = searchTerms.map(kw => (kw, p.delimContents.indexOf(kw))).filter(_._2 >= 0).toList
+            if (indexes.nonEmpty) {
+                val dist = distCalc(searchTerms.toSeq.toList, indexes, 0, 0, indexes.head._2, 0)
+                if (dist > 0) distances = (p, dist) :: distances
+            }
+        })
 
-            distances.sortWith(_._2 < _._2)
-        } else {
-            pageStore.pages.foreach(p => {
-                val what = p.delimContents.count(_ == searchTerms(0))
-                if (what > 0) distances = (p, what) :: distances
-            })
+        distances.sortWith(_._2 < _._2)
+    }
 
-            distances.sortWith(_._2 > _._2)
-        }
+    val findSingleWord = (pages: Set[Page], word: String) => {
+        var occurances: List[(Page, Int)] = List()
+
+        pages.foreach(p => {
+            val c = p.delimContents.count(_ == word)
+            if (c > 0) occurances = (p, c) :: occurances
+        })
+
+        occurances.sortWith(_._2 > _._2)
     }
 
     def distCalc(terms: List[String], found: List[(String, Int)], termPointer: Int, foundPointer: Int, pointer: Int, distance: Int): Int = {
